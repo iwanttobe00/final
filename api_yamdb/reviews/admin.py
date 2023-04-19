e027a6589f6bcf32fc2a63cbb17d913f346d109c
iwanttobe00 <xxhh0@mail.ru>
2023-04-19 20:57:09 +0400

    sss

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,43 +1,8 @@
 from django.contrib import admin
+from reviews.models import Category, Comment, Genre, Review, Title
 
-from .models import Category, Comment, Genre, Review, Title, User
-
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'email', 'role')
-    list_editable = ('role',)
-    list_filter = ('role',)
-    ordering = ('id',)
-
-
-class CategoryGenreAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'slug')
-    list_editable = ('name', 'slug')
-    ordering = ('id',)
-
-
-class TitleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'year', 'category')
-    list_editable = ('year', 'category')
-    list_filter = ('category', 'year')
-    ordering = ('id',)
-
-
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('id', 'text', 'title', 'author', 'score')
-    list_filter = ('author',)
-    ordering = ('id',)
-
-
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'text', 'review', 'author',)
-    list_filter = ('author',)
-    ordering = ('id',)
-
-
-admin.site.register(User, UserAdmin)
-admin.site.register(Category, CategoryGenreAdmin)
-admin.site.register(Genre, CategoryGenreAdmin)
-admin.site.register(Title, TitleAdmin)
-admin.site.register(Review, ReviewAdmin)
-admin.site.register(Comment, CommentAdmin)
+admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Title)
+admin.site.register(Review)
+admin.site.register(Comment)
